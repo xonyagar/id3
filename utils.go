a41b380032cc73f31181a4dbc7c9d0abeecb73ef
Naser Mirzaei <nasermirzaei89@gmail.com>
2018-03-25 20:24:09 +0430

    add multiframe support to v22

--- utils.go
+++ utils.go
@@ -2,7 +2,7 @@ package id3
 
 import "strings"
 
-const invalidChars  = string(uint(0))+string(uint(1))+" "
+const invalidChars = string(uint(0)) + string(uint(1)) + " "
 
 func trim(s string) string {
 	return strings.Trim(s, invalidChars)
--- v22.go
+++ v22.go
@@ -1,15 +1,13 @@
 package id3
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
-	"io"
 	"image"
 	"image/jpeg"
-	"bytes"
 	"image/png"
-	"strings"
-	"strconv"
+	"io"
 )
 
 // V1HeaderSize is size of ID3v2.2, ID3v2.3 and ID3v2.4 tag header
@@ -21,14 +19,200 @@ const V2FrameHeaderSize = 6
 type FrameType int
 
 const (
-	FrameText          FrameType = iota
-	FrameNumber
-	FrameImage
-	FrameGenre
-	FrameTrackPosition
-	FrameLang
-	FrameBool
+	TypeUnknown FrameType = iota
+	TypeUniqueFileIdentifier
+	TypeTextInformation
+	TypeURLLink
+	TypeInvolvedPeopleList
+	TypeMusicCDIdentifier
+	TypeEventTimingCodes
+	TypeMPEGLocationLookupTable
+	TypeSyncedTempoCodes
+	TypeUnsychronisedLyricsOrTextTranscription
+	TypeSynchronisedLyricsOrText
+	TypeComments
+	TypeRelativeVolumeAdjustment
+	TypeEqualisation
+	TypeReverb
+	TypeAttachedPicture
+	TypeGeneralEncapsulatedObject
+	TypePlayCounter
+	TypePopularimeter
+	TypeRecommendedBufferSize
+	TypeEncryptedMetaFrame
+	TypeAudioEncryption
+	TypeLinkedInformation
 )
+
+type Frame interface {
+	ID() string
+	Size() int
+}
+
+type frameBase struct {
+	id   string
+	size int
+}
+
+func (f frameBase) ID() string {
+	return f.id
+}
+
+func (f frameBase) Size() int {
+	return f.size
+}
+
+type UnknownFrame struct {
+	frameBase
+	data []byte
+}
+
+func (f UnknownFrame) Data() []byte {
+	return f.data
+}
+
+type UniqueFileIdentifierFrame struct {
+	frameBase
+	ownerIdentifier string
+	identifier      []byte
+}
+
+func (f UniqueFileIdentifierFrame) OwnerIdentifier() string {
+	return f.ownerIdentifier
+}
+
+func (f UniqueFileIdentifierFrame) Identifier() []byte {
+	return f.identifier
+}
+
+type TextInformationFrame struct {
+	frameBase
+	encoding Encoding
+	text     string
+}
+
+func (f TextInformationFrame) Text() string {
+	return f.text
+}
+
+type InvolvedPeopleListFrame struct {
+	frameBase
+	encoding   Encoding
+	peopleList []string
+}
+
+func (f InvolvedPeopleListFrame) PeopleList() []string {
+	return f.peopleList
+}
+
+type URLLinkFrame struct {
+	frameBase
+	url string
+}
+
+func (f URLLinkFrame) URL() string {
+	return f.url
+}
+
+type MusicCDIdentifierFrame struct {
+	frameBase
+	cdTOC []byte
+}
+
+func (f MusicCDIdentifierFrame) CDTOC() []byte {
+	return f.cdTOC
+}
+
+type TimeStampFormat byte
+
+type EventTimingCodesFrame struct {
+	frameBase
+	timeStampFormat TimeStampFormat
+}
+
+func (f EventTimingCodesFrame) TimeStampFormat() TimeStampFormat {
+	return f.timeStampFormat
+}
+
+// 4.7.   MPEG location lookup table
+
+// 4.8.   Synced tempo codes
+
+// 4.9.   Unsychronised lyrics/text transcription
+
+// 4.10.   Synchronised lyrics/text
+
+// 4.11.   Comments
+
+// 4.12.   Relative volume adjustment
+
+// 4.13.   Equalisation
+
+// 4.14.   Reverb
+
+type PictureType int
+
+const (
+	PictureTypeOther PictureType = iota
+	PictureType32x32
+	PictureTypeOtherFileIcon
+	PictureTypeCoverFront
+	PictureTypeCoverBack
+	PictureTypeLeafletPage
+	PictureTypeMedia
+	PictureTypeLeadArtist
+	PictureTypeArtist
+	PictureTypeConductor
+	PictureTypeBandOrOrchestra
+	PictureTypeComposer
+	PictureTypeLyricist
+	PictureTypeRecordingLocation
+	PictureTypeDuringRecording
+	PictureTypeDuringPerformance
+	PictureTypeMovieOrVideoScreenCapture
+	PictureTypeABrightColouredFish
+	PictureTypeIllustration
+	PictureTypeBandOrArtistLogotype
+	PictureTypePublisherOrStudioLogotype
+)
+
+type AttachedPictureFrame struct {
+	frameBase
+	textEncoding Encoding
+	imageFormat  string
+	pictureType  PictureType
+	description  string
+	pictureData  []byte
+}
+
+func (f AttachedPictureFrame) Image() (image.Image, error) {
+	switch f.imageFormat {
+	case "JPG":
+		return jpeg.Decode(bytes.NewReader(f.pictureData))
+	case "PNG":
+		return png.Decode(bytes.NewReader(f.pictureData))
+	default:
+		return nil, errors.New("invalid image format")
+	}
+}
+
+func (f AttachedPictureFrame) Description() string {
+	return f.description
+}
+
+// 4.16.   General encapsulated object
+
+// 4.17.   Play counter
+
+// 4.18.   Popularimeter
+
+// 4.19.   Recommended buffer size
+
+// 4.20.   Encrypted meta frame
+
+// 4.21.   Audio encryption
+
+// 4.22.   Linked information
 
 type DeclaredFrame struct {
 	ID          string
@@ -36,82 +220,84 @@ type DeclaredFrame struct {
 	Type        FrameType
 }
 
-var V22Frames = map[string]DeclaredFrame{
-	"BUF": {"BUF", "Recommended buffer size", FrameText},
-	"CNT": {"CNT", "Play counter", FrameText},
-	"COM": {"COM", "Comments", FrameLang},
-	"CRA": {"CRA", "Audio encryption", FrameText},
-	"CRM": {"CRM", "Encrypted meta frame", FrameText},
-	"ETC": {"ETC", "Event timing codes", FrameText},
-	"EQU": {"EQU", "Equalization", FrameText},
-	"GEO": {"GEO", "General encapsulated object", FrameText},
-	"IPL": {"IPL", "Involved people list", FrameText},
-	"LNK": {"LNK", "Linked information", FrameText},
-	"MCI": {"MCI", "Music CD Identifier", FrameText},
-	"MLL": {"MLL", "MPEG location lookup table", FrameText},
-	"PIC": {"PIC", "Attached picture", FrameImage},
-	"POP": {"POP", "Popularimeter", FrameText},
-	"REV": {"REV", "Reverb", FrameText},
-	"RVA": {"RVA", "Relative volume adjustment", FrameText},
-	"SLT": {"SLT", "Synchronized lyric/text", FrameText},
-	"STC": {"STC", "Synced tempo codes", FrameText},
-	"TAL": {"TAL", "Album/Movie/Show title", FrameText},
-	"TBP": {"TBP", "BPM (Beats Per Minute)", FrameText},
-	"TCM": {"TCM", "Composer", FrameText},
-	"TCO": {"TCO", "Content type", FrameGenre},
-	"TCR": {"TCR", "Copyright message", FrameText},
-	"TDA": {"TDA", "Date", FrameText},
-	"TDY": {"TDY", "Playlist delay", FrameText},
-	"TEN": {"TEN", "Encoded by", FrameText},
-	"TFT": {"TFT", "File type", FrameText},
-	"TIM": {"TIM", "Time", FrameText},
-	"TKE": {"TKE", "Initial key", FrameText},
-	"TLA": {"TLA", "Language(s)", FrameText},
-	"TLE": {"TLE", "Length", FrameNumber},
-	"TMT": {"TMT", "Media type", FrameText},
-	"TOA": {"TOA", "Original artist(s)/performer(s)", FrameText},
-	"TOF": {"TOF", "Original filename", FrameText},
-	"TOL": {"TOL", "Original Lyricist(s)/text writer(s)", FrameText},
-	"TOR": {"TOR", "Original release year", FrameText},
-	"TOT": {"TOT", "Original album/Movie/Show title", FrameText},
-	"TP1": {"TP1", "Lead artist(s)/Lead performer(s)/Soloist(s)/Performing group", FrameText},
-	"TP2": {"TP2", "Band/Orchestra/Accompaniment", FrameText},
-	"TP3": {"TP3", "Conductor/Performer refinement", FrameText},
-	"TP4": {"TP4", "Interpreted, remixed, or otherwise modified by", FrameText},
-	"TPA": {"TPA", "Part of a set", FrameText},
-	"TPB": {"TPB", "Publisher", FrameText},
-	"TRC": {"TRC", "ISRC (International Standard Recording Code)", FrameText},
-	"TRD": {"TRD", "Recording dates", FrameText},
-	"TRK": {"TRK", "Track number/Position in set", FrameTrackPosition},
-	"TSI": {"TSI", "Size", FrameText},
-	"TSS": {"TSS", "Software/hardware and settings used for encoding", FrameText},
-	"TT1": {"TT1", "Content group description", FrameText},
-	"TT2": {"TT2", "Title/Songname/Content description", FrameText},
-	"TT3": {"TT3", "Subtitle/Description refinement", FrameText},
-	"TXT": {"TXT", "Lyricist/text writer", FrameText},
-	"TXX": {"TXX", "User defined text information frame", FrameText},
-	"TYE": {"TYE", "Year", FrameText},
-	"UFI": {"UFI", "Unique file identifier", FrameText},
-	"ULT": {"ULT", "Unsychronized lyric/text transcription", FrameLang},
-	"WAF": {"WAF", "Official audio file webpage", FrameText},
-	"WAR": {"WAR", "Official artist/performer webpage", FrameText},
-	"WAS": {"WAS", "Official audio source webpage", FrameText},
-	"WCM": {"WCM", "Commercial information", FrameText},
-	"WCP": {"WCP", "Copyright/Legal information", FrameText},
-	"WPB": {"WPB", "Publishers official webpage", FrameText},
-	"WXX": {"WXX", "User defined URL link frame", FrameText},
-	// iTunes
-	"TCP": {"TCP", "Part of a compilation", FrameBool},
+var V22DeclaredFrames = map[string]DeclaredFrame{
+	"BUF": {"BUF", "Recommended buffer size", TypeUnknown},
+	"CNT": {"CNT", "Play counter", TypeUnknown},
+	"COM": {"COM", "Comments", TypeUnknown},
+	"CRA": {"CRA", "Audio encryption", TypeUnknown},
+	"CRM": {"CRM", "Encrypted meta frame", TypeUnknown},
+	"ETC": {"ETC", "Event timing codes", TypeUnknown},
+	"EQU": {"EQU", "Equalization", TypeUnknown},
+	"GEO": {"GEO", "General encapsulated object", TypeUnknown},
+	"IPL": {"IPL", "Involved people list", TypeInvolvedPeopleList},
+	"LNK": {"LNK", "Linked information", TypeUnknown},
+	"MCI": {"MCI", "Music CD Identifier", TypeUnknown},
+	"MLL": {"MLL", "MPEG location lookup table", TypeUnknown},
+	"PIC": {"PIC", "Attached picture", TypeAttachedPicture},
+	"POP": {"POP", "Popularimeter", TypeUnknown},
+	"REV": {"REV", "Reverb", TypeUnknown},
+	"RVA": {"RVA", "Relative volume adjustment", TypeUnknown},
+	"SLT": {"SLT", "Synchronized lyric/text", TypeUnknown},
+	"STC": {"STC", "Synced tempo codes", TypeUnknown},
+
+	"TAL": {"TAL", "Album/Movie/Show title", TypeTextInformation},
+	"TBP": {"TBP", "BPM (Beats Per Minute)", TypeTextInformation},
+	"TCM": {"TCM", "Composer", TypeTextInformation},
+	"TCO": {"TCO", "Content type", TypeTextInformation},
+	"TCR": {"TCR", "Copyright message", TypeTextInformation},
+	"TDA": {"TDA", "Date", TypeTextInformation},
+	"TDY": {"TDY", "Playlist delay", TypeTextInformation},
+	"TEN": {"TEN", "Encoded by", TypeTextInformation},
+	"TFT": {"TFT", "File type", TypeTextInformation},
+	"TIM": {"TIM", "Time", TypeTextInformation},
+	"TKE": {"TKE", "Initial key", TypeTextInformation},
+	"TLA": {"TLA", "Language(s)", TypeTextInformation},
+	"TLE": {"TLE", "Length", TypeTextInformation},
+	"TMT": {"TMT", "Media type", TypeTextInformation},
+	"TOA": {"TOA", "Original artist(s)/performer(s)", TypeTextInformation},
+	"TOF": {"TOF", "Original filename", TypeTextInformation},
+	"TOL": {"TOL", "Original Lyricist(s)/text writer(s)", TypeTextInformation},
+	"TOR": {"TOR", "Original release year", TypeTextInformation},
+	"TOT": {"TOT", "Original album/Movie/Show title", TypeTextInformation},
+	"TP1": {"TP1", "Lead artist(s)/Lead performer(s)/Soloist(s)/Performing group", TypeTextInformation},
+	"TP2": {"TP2", "Band/Orchestra/Accompaniment", TypeTextInformation},
+	"TP3": {"TP3", "Conductor/Performer refinement", TypeTextInformation},
+	"TP4": {"TP4", "Interpreted, remixed, or otherwise modified by", TypeTextInformation},
+	"TPA": {"TPA", "Part of a set", TypeTextInformation},
+	"TPB": {"TPB", "Publisher", TypeTextInformation},
+	"TRC": {"TRC", "ISRC (International Standard Recording Code)", TypeTextInformation},
+	"TRD": {"TRD", "Recording dates", TypeTextInformation},
+	"TRK": {"TRK", "Track number/Position in set", TypeTextInformation},
+	"TSI": {"TSI", "Size", TypeTextInformation},
+	"TSS": {"TSS", "Software/hardware and settings used for encoding", TypeTextInformation},
+	"TT1": {"TT1", "Content group description", TypeTextInformation},
+	"TT2": {"TT2", "Title/Songname/Content description", TypeTextInformation},
+	"TT3": {"TT3", "Subtitle/Description refinement", TypeTextInformation},
+	"TXT": {"TXT", "Lyricist/text writer", TypeTextInformation},
+	"TXX": {"TXX", "User defined text information frame", TypeUnknown},
+	"TYE": {"TYE", "Year", TypeTextInformation},
+	"TCP": {"TCP", "Part of a compilation", TypeUnknown}, // iTunes
+
+	"UFI": {"UFI", "Unique file identifier", TypeUniqueFileIdentifier},
+	"ULT": {"ULT", "Unsychronized lyric/text transcription", TypeUnknown},
+
+	"WAF": {"WAF", "Official audio file webpage", TypeURLLink},
+	"WAR": {"WAR", "Official artist/performer webpage", TypeURLLink},
+	"WAS": {"WAS", "Official audio source webpage", TypeURLLink},
+	"WCM": {"WCM", "Commercial information", TypeURLLink},
+	"WCP": {"WCP", "Copyright/Legal information", TypeURLLink},
+	"WPB": {"WPB", "Publishers official webpage", TypeURLLink},
+	"WXX": {"WXX", "User defined URL link frame", TypeUnknown},
 }
 
 // V22 is ID3v2.2 tag reader
 type V22 struct {
-	frames map[string]interface{}
+	frames []Frame
 }
 
 // NewID3V22 will read file and return id3v2.2 tag reader
 func NewID3V22(f io.ReadSeeker) (*V22, error) {
-	header := make([]byte, 10)
+	header := make([]byte, V2HeaderSize)
 	n, err := f.Read(header)
 	if err != nil {
 		return nil, err
@@ -129,28 +315,81 @@ func NewID3V22(f io.ReadSeeker) (*V22, error) {
 		return nil, errors.New("file id3v2 version is not 2.2.0")
 	}
 
-	frames := map[string]interface{}{}
-	frmsSize := int(header[9]) + int(header[8])*128 + int(header[7])*128*128 + int(header[6])*128*128*128
+	frames := make([]Frame, 0)
+	framesSize := int(uint32(header[9]) + uint32(header[8])<<8 + uint32(header[7])<<16 + uint32(header[6])<<32)
 
-	for t := 0; t < frmsSize; {
-		frmHeader := make([]byte, V2FrameHeaderSize)
-		n, err = f.Read(frmHeader)
+	for t := 0; t < framesSize; {
+		frameHeader := make([]byte, V2FrameHeaderSize)
+		n, err = f.Read(frameHeader)
 		if err != nil {
 			return nil, err
 		}
-		if frmHeader[0]+frmHeader[1]+frmHeader[2] == 0 {
+		// FIXME
+		if frameHeader[0]+frameHeader[1]+frameHeader[2] == 0 {
 			break
 		}
 		t += n
 
-		frmSize := uint32(frmHeader[5]) + uint32(frmHeader[4])<<8 + uint32(frmHeader[3])<<16
-		frmBody := make([]byte, frmSize)
-		n, err = f.Read(frmBody)
+		frameID := string(frameHeader[:3])
+		frameSize := int(uint32(frameHeader[5]) + uint32(frameHeader[4])<<8 + uint32(frameHeader[3])<<16)
+		frameBody := make([]byte, frameSize)
+		n, err = f.Read(frameBody)
 		if err != nil {
 			return nil, err
 		}
 		t += n
-		frames[string(frmHeader[0:3])] = frmBody
+
+		frameBase := frameBase{
+			id:   frameID,
+			size: frameSize,
+		}
+
+		df, ok := V22DeclaredFrames[string(frameID)]
+		if !ok {
+			frame := UnknownFrame{
+				frameBase: frameBase,
+				data:      frameBody,
+			}
+			frames = append(frames, frame)
+			continue
+		}
+
+		switch df.Type {
+		case TypeTextInformation:
+			frame := TextInformationFrame{
+				frameBase: frameBase,
+				encoding:  Encoding(frameBody[0]),
+				text:      toUTF8(frameBody[1:], Encoding(frameBody[0])),
+			}
+			frames = append(frames, frame)
+		case TypeURLLink:
+			frame := URLLinkFrame{
+				frameBase: frameBase,
+				url:       string(frameBody),
+			}
+			frames = append(frames, frame)
+		case TypeAttachedPicture:
+			frame := AttachedPictureFrame{
+				frameBase:    frameBase,
+				textEncoding: Encoding(frameBody[0]),
+				imageFormat:  string(frameBody[1:4]),
+				pictureType:  PictureType(frameBody[4]),
+			}
+			for i := 5; i < frameSize; i++ {
+				if frameBody[i] == 0 {
+					frame.description = toUTF8(frameBody[5:i], frame.textEncoding)
+					frame.pictureData = frameBody[i+1:]
+					break
+				}
+			}
+			frames = append(frames, frame)
+		default:
+			frame := UnknownFrame{
+				frameBase: frameBase,
+				data:      frameBody,
+			}
+			frames = append(frames, frame)
+		}
 	}
 
 	tag := new(V22)
@@ -158,99 +397,6 @@ func NewID3V22(f io.ReadSeeker) (*V22, error) {
 	return tag, nil
 }
 
-func (tag V22) ListFrames() []string {
-	keys := make([]string, 0, len(tag.frames))
-	for k := range tag.frames {
-		keys = append(keys, k)
-	}
-
-	return keys
-}
-
-func (tag V22) TextFrame(id string) string {
-	if frm, ok := tag.frames[id]; ok {
-		return trim(string(frm.([]uint8)))
-	}
-
-	return ""
-}
-
-func (tag V22) BoolFrame(id string) bool {
-	if frm, ok := tag.frames[id]; ok {
-		return trim(string(frm.([]uint8))) == "1"
-	}
-
-	return false
-}
-
-func (tag V22) LangFrame(id string) (string, string) {
-	if frm, ok := tag.frames[id]; ok {
-		t := trim(string(frm.([]uint8)))
-		return t[:3], t[3:]
-	}
-
-	return "", ""
-}
-
-func (tag V22) NumberFrame(id string) int {
-	if frm, ok := tag.frames[id]; ok {
-		n, err := strconv.Atoi(trim(string(frm.([]uint8))))
-		if err == nil {
-			return n
-		}
-	}
-
-	return 0
-}
-
-func (tag V22) TrackPositionFrame(id string) (int, int) {
-	if frm, ok := tag.frames[id]; ok {
-		s := strings.Split(trim(string(frm.([]uint8))), "/")
-		trk, err := strconv.Atoi(s[0])
-		if err != nil {
-			return 0, 0
-		}
-
-		if len(s) == 2 {
-			psn, err := strconv.Atoi(s[1])
-			if err != nil {
-				return trk, 0
-			}
-
-			return trk, psn
-		}
-
-		return trk, 0
-	}
-
-	return 0, 0
-}
-
-func (tag V22) GenreFrame(id string) string {
-	if frm, ok := tag.frames[id]; ok {
-		gids := strings.Trim(trim(string(frm.([]uint8))), "()")
-		gid, err := strconv.Atoi(gids)
-		if err == nil {
-			if gid < len(V1Genres) {
-				return V1Genres[gid]
-			}
-		}
-	}
-
-	return ""
-}
-
-func (tag V22) ImageFrame(id string) (image.Image, error) {
-	if frm, ok := tag.frames[id]; ok {
-		switch string(frm.([]byte)[1:4]) {
-		case "JPG":
-			return jpeg.Decode(bytes.NewReader(frm.([]byte)[6:]))
-		case "PNG":
-			return png.Decode(bytes.NewReader(frm.([]byte)[6:]))
-		default:
-			return nil, errors.New("invalid image type")
-		}
-	}
-
-	return nil, errors.New("frame not found")
+func (tag V22) Frames() []Frame {
+	return tag.frames
 }
